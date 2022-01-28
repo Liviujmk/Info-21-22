d5b3928288e33d15f34bf27b00a00429e8bd599c
Liviu MItrofan <64259428+Liviujmk@users.noreply.github.com>
2022-01-28 00:22:23 -0800

    Update 27.01.cpp

--- Lab-info/27.01.cpp
+++ Lab-info/27.01.cpp
@@ -25,3 +25,61 @@ int main()
     cout<<s<<" "<<n;
     return 0;
 }
+
+
+// v2.III.1
+
+#include <iostream>
+
+using namespace std;
+
+int factori (int n, int m){
+    int k1, k2, nr=0;
+    int d=2;
+    while(n!=1){
+        k1=0;
+        while(n%d==0){
+            n/=d;
+            k1++;
+        }
+        k2=0;
+        while(m%d==0){
+            m/=d;
+            k2++;
+        }
+        d++;
+        if(k1==k2)
+            nr++;
+    }
+    return nr;
+}
+
+int main()
+{
+    cout<< factori(16500, 10780);
+    return 0;
+}
+
+//v2.III.2
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[100][100];
+    int n=5;
+    for(int i=1;i<=n;i++)
+        for(int j=1;j<=n;j++)
+            if(i+j <= n+1)
+                a[i][j]=j;
+            else
+                a[i][j]=a[n+1-j][n+1-i];
+    for(int i=1;i<=n;i++)
+        for(int j=1;j<=n;j++)
+            cout<<a[i][j]<<endl;
+    cout<<endl;
+    return 0;
+}
+????
