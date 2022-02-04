2de5e898e56ea7d0d1269106cd413bdf134c769f
Liviu MItrofan <64259428+Liviujmk@users.noreply.github.com>
2022-02-03 22:01:56 -0800

    Create 3.02.cpp

---
+++ Lab-info/3.02.cpp
@@ -0,0 +1,21 @@
+// antrenament v2.2.3
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    while(f>>x){
+        n++;
+        if(x>0)
+        if(kp=-1)
+            kp=n;
+        else
+            kn=n;
+    }
+    if(n-kp+1 > kn)
+        cout<<n-kp+1;
+    else
+        cout<<kn;
+    return 0;
+}
