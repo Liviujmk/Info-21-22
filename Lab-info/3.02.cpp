961fdd17dc3f255a22f75bf2a79e9d02719efb6e
Liviu MItrofan <64259428+Liviujmk@users.noreply.github.com>
2022-02-03 23:45:19 -0800

    Update 3.02.cpp

--- Lab-info/3.02.cpp
+++ Lab-info/3.02.cpp
@@ -19,3 +19,40 @@ int main()
         cout<<kn;
     return 0;
 }
+
+
+// model 2022
+//III.1
+
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int mozaic(int x, int y, int z){
+    int a,b,c,cm,cmf;
+    a=x;
+    b=y;
+    c=z;
+    while(a!=b){
+        if(a>b)
+            a-=b;
+        else
+            b--=a;
+    }
+    cm=(x*y)/a;
+    int cmm=cm;
+    while(cm!c){
+        if(cm>c)
+            cm-=c;
+        else
+            c-=cm;
+    }
+    cmf = (cmm*z)/c;
+    return cmf;
+}
+int main (){
+
+    return 0;
+}
