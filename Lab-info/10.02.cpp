ab2479bc173085fee54adca82ba31913ae36113c
Liviu MItrofan <64259428+Liviujmk@users.noreply.github.com>
2022-02-11 00:15:40 -0800

    Update 10.02.cpp

--- Lab-info/10.02.cpp
+++ Lab-info/10.02.cpp
@@ -34,3 +34,42 @@ int main()
     f.close();
     return 0;
 }
+
+/////Test 10 antrenament 2021
+///III.2
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+ifstream f("cif.txt");
+
+
+int main()
+{
+    int m=5, n=4,a[100][100];
+    for(int i=1;i<=m;i++)
+        for(int j=1;j<=n;j++)
+            cin>>a[i][j];
+    
+    int gasit = 0;
+    for(int j=1;j<=n-1;j++){
+        for(int i=1;i<=m;i++)
+            if(a[i][j]==a[i][m]){
+                cout<<a[i][j]<<" ";
+                gasit = 1;
+            }   
+    }
+    if(gasit==0)
+        cout<<"nu exista";
+    return 0;
+}
+
+/// III.3
+int p,x,k=1;
+while(f>>x)
+    k++;
+cout<<k;
+f.close();
+return 0;}
