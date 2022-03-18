ab9013dc50c384df8d12adc44ab6c1034e9221ce
Liviu Mitrofan <64259428+Liviujmk@users.noreply.github.com>
2022-03-17 23:18:53 -0700

    Create august.cpp

---
+++ Lab-info/BAC/2021/august.cpp
@@ -0,0 +1,15 @@
+II.1. 
+  a) 3,4,6,8,9,15
+  
+  2. ARMONIA
+  3. a[0].venit = 4000
+     a[0].dp.anNastere = 2000
+ 
+III
+  1. void cuburi(unsigned int n){
+      for(int i=n; i>=1; i--)
+        cout<<i*i*i<<" ";
+      }
+  
+  2. 
+  
