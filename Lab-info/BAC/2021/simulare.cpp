b5acb7d00af7b61cbac81f19e3bef9dffb7b2d9a
Liviu Mitrofan <64259428+Liviujmk@users.noreply.github.com>
2022-03-24 21:58:50 +0100

    Update simulare.cpp

--- Lab-info/BAC/2021/simulare.cpp
+++ Lab-info/BAC/2021/simulare.cpp
@@ -7,3 +7,28 @@ I.1. c
 II.1.a.
      b.
      
+III.1
+  #include <iostream>
+  using namespace std;
+  int n,d,p;
+  int main()
+  {
+    cout<<"Introduceti numarul n: ";
+    cin>>n;
+    d=2;
+    while(n>1)
+    {
+            p=0;
+            while(n%d==0)
+            {
+                p++;
+                n=n/d;
+            }
+            if(p==1){
+                cout<<d<<"^"<<p<<" ";
+                break;
+            }
+            d=d+1;
+    }
+    return 0;
+    }
