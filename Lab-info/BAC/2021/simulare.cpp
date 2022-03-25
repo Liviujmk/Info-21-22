17f0268720fe3a65efc0a4cd7234f3c67acd87a7
Liviu Mitrofan <64259428+Liviujmk@users.noreply.github.com>
2022-03-24 23:06:32 -0700

    Update simulare.cpp

--- Lab-info/BAC/2021/simulare.cpp
+++ Lab-info/BAC/2021/simulare.cpp
@@ -8,30 +8,23 @@ II.1.a.
      b.
      
 III.1
-  #include <iostream>
-  using namespace std;
-  int n,d,p;
-  int main()
-  {
-    cout<<"Introduceti numarul n: ";
-    cin>>n;
-    d=2;
-    while(n>1)
-    {
-            p=0;
-            while(n%d==0)
-            {
-                p++;
-                n=n/d;
-            }
-            if(p==1){
-                cout<<d<<"^"<<p<<" ";
-                break;
-            }
-            d=d+1;
+  int putere(unsigned int n){
+    int d=2,p,pMin, dMin;
+    pMin = 100000;
+    while(n>1){
+        p=0;
+        while(n%d==0){
+            p++;
+            n/=d;
+        }
+        if(p!=0 && p<pMin){
+            pMin = p;
+            dMin = d;
+        }
+        d++;
     }
-    return 0;
-    }
+    return dMin;
+  }  
 
 III.2
   #include <iostream>
