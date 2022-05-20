5490140086e12f27c650979abd58f5fb4c862455
Liviu Mitrofan <64259428+Liviujmk@users.noreply.github.com>
2022-05-19 21:41:23 -0900

    Update 807.cpp

--- Variante/ebacalaureat/2014/807.cpp
+++ Variante/ebacalaureat/2014/807.cpp
@@ -20,3 +20,41 @@ int main()
     return 0;
 }
 
+III
+
+#include <iostream>
+
+using namespace std;
+
+int divizor(int n, int &d){
+    int p,div;
+    div=2;
+    while(n>1){
+        p=0;
+        while(n%div==0){
+            p++;
+            n/=div;
+            d=div;
+        }
+        div++;
+    }
+}
+
+int main()
+{
+    /*int d;
+    divizor(40, d);
+    cout<<d;*/
+    int x, val=0, n=6,y;
+    cin>>x;
+    for(int i=2;i<=n/2+1;i++){
+        cin>>y;
+        if(x!=y)
+            val=x;
+    } x=y;
+    if(val==0)
+        cout<<"Nu";
+    else
+        cout<<val;
+    return 0;
+}
